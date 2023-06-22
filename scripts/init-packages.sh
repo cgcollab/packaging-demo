6c561ce19a2e2662eb7525bb9fa0ce79ed8cacdd
Cora Iberkleid <ciberkleid@gmail.com>
2023-06-22 13:12:59 -0400

    typo corrections and cleanup

--- scripts/init-packages.sh
+++ scripts/init-packages.sh
@@ -4,18 +4,21 @@ export MY_REG=gcr.io/fe-ciberkleid/packaging-demo
 myDest=$MY_REG/hello-app yq e -i '.destinations[0].newImage = env(myDest)' packages/hello-app/config/kbld.yml
 mkdir -p tmp/src/hello-app && rm -rf tmp/src/hello-app && git clone https://github.com/cgcollab/hello-app tmp/src/hello-app
 kctrl package init --chdir packages/hello-app # Prompts: hello-app.corp.com,1,config
+# The next command is a bit slow because it runs pack to build the app container
 kctrl package release --chdir packages/hello-app --version 1.2.3 --repo-output ../../repository/1.0.0 # Prompts: <YOUR REG + "/hello-app">
 
 # redis - init & release package
 kctrl package init --chdir packages/redis # Prompts: redis.corp.com,4,https://github.com/cgcollab/redis,origin/main,redis-*
-# manuallly add overlays directory to be included in package and imgpkg (CRD ref: https://carvel.dev/kapp-controller/docs/v0.46.0/packaging/#package-build)
-yq eval -i '.spec.template.spec.app.spec.template[0].ytt.paths += ["overlays"]' packages/redis/package-build.yml
-yq eval -i '.spec.template.spec.export[0].includePaths += ["overlays"]' packages/redis/package-build.yml
+# manually add overlays directory to be included in package and imgpkg (CRD ref: https://carvel.dev/kapp-controller/docs/v0.46.0/packaging/#package-build)
+yq eval -i '.spec.template.spec.app.spec.template[0].ytt.paths += ["overlays"] | .spec.template.spec.app.spec.template[0].ytt.paths = (.spec.template.spec.app.spec.template[0].ytt.paths | unique)' packages/redis/package-build.yml
+yq eval -i '.spec.template.spec.export[0].includePaths += ["overlays"] | .spec.template.spec.export[0].includePaths = (.spec.template.spec.export[0].includePaths | unique)' packages/redis/package-build.yml
+# Run init again after yq changes, ANSWER 1 IN THE FINAL PROMPT TO AUTOMATICALLY UPDATE package-resources.yml based on changes made to package-build.yml
+kctrl package init --chdir packages/redis # Prompts: redis.corp.com,4,https://github.com/cgcollab/redis,origin/main,redis-*,1
 kctrl package release --chdir packages/redis --version 2.1.0 --repo-output ../../repository/1.0.0 # Prompts: <YOUR REG + "/redis">
 
-# metapackage (use same version for repo and metapackage) - init & release package
+# metapackage (append metapackage version to repo-output path) - init & release package
 kctrl package init --chdir packages/metapackage # Prompts: metapackage.corp.com,1,config
 kctrl package release --chdir packages/metapackage --version 1.0.0 --repo-output ../../repository/1.0.0 # Prompts: <YOUR REG + "/metapackage">
 
-# Release repository (use same version for repo and metapackage)
+# Release repository (use same version that was used for the metapackage)
 kctrl package repository release --chdir repository/1.0.0 --version 1.0.0  # Prompts: metapackage-repo.corp.com,<YOUR REG + "/metapackage-repo">
--- scripts/install-metapackage.sh
+++ scripts/install-metapackage.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+export MY_REG=gcr.io/fe-ciberkleid/packaging-demo
+
+# kind delete cluster
 # kind create cluster
 # kapp deploy -a kapp-controller -f https://github.com/vmware-tanzu/carvel-kapp-controller/releases/latest/download/release.yml -y
 
 kubectl create ns carvel-repos
-kctrl package repo add -r metapackage-repo --url gcr.io/fe-ciberkleid/metapackage-repo:1.0.0 -n carvel-repos
+kctrl package repo add -r metapackage-repo --url $MY_REG/metapackage-repo:1.0.0 -n carvel-repos
 kctrl package repository list -A
 
 kctrl package available list -A # shorthand: kctrl p a ls -A
@@ -12,15 +15,14 @@ kctrl package available get -p metapackage.corp.com -n carvel-repos
 kctrl package available get -p metapackage.corp.com/1.0.0 --values-schema -n carvel-repos
 
 kubectl create ns hello-app
-kubectl create ns metapackage-app
-kctrl package install -i my-metapackage -p metapackage.corp.com -v 1.0.0 -n metapackage-install
+kctrl package install -i my-metapackage -p metapackage.corp.com -v 1.0.0 # Should create and default to namespace "metapackage-install"
 
 # TROUBLESHOOTING
 # In case of error, correct configuration errors and re-release corrected package, metapackage, and repo by re-running init commands with a "-y" flag
 # See scripts/update-packages.sh
 
 # To delete a package:
-# kctrl p i delete -i my-app -n installs
+# kctrl p i delete -i my-metapackage -n metapackage-install
 # If deletion gets stuck, in separate terminal, run:
-# kctrl p i delete -i redis -n installs --noop
-# kctrl p i delete -i hello-app -n installs --noop
+# kctrl p i delete -i redis -n metapackage-install --noop
+# kctrl p i delete -i hello-app -n metapackage-install --noop
