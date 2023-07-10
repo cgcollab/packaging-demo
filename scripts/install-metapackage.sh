32365ec30130b6e70eaf86e328428982c42ee2f9
Cora Iberkleid <ciberkleid@gmail.com>
2023-07-10 12:47:25 -0400

    use kn cli to create kind cluster with knative serving

--- scripts/install-metapackage.sh
+++ scripts/install-metapackage.sh
@@ -3,9 +3,15 @@
 if [[ $(basename $(pwd)) == "scripts" ]]; then cd .. && echo "PWD=$(pwd)"; fi
 if [[ -z $MY_REG ]]; then echo "ERROR: Env is not set. Exiting script."; (return 0 2>/dev/null) && return || exit; fi
 
-# kind delete cluster
-# kind create cluster
-# kapp deploy -a kapp-controller -f https://github.com/vmware-tanzu/carvel-kapp-controller/releases/latest/download/release.yml -y
+# Reference: https://knative.dev/docs/install/quickstart-install/#run-the-knative-quickstart-plugin
+# One time:
+### brew install knative/client/kn
+### brew install knative-sandbox/kn-plugins/quickstart
+kind delete cluster --name pkg-demo
+kn quickstart kind --name pkg-demo --install-serving
+### kind delete cluster
+### kind create cluster
+kapp deploy -a kapp-controller -f https://github.com/vmware-tanzu/carvel-kapp-controller/releases/latest/download/release.yml -y
 
 kubectl create ns metapackage-install
 kctrl package repo add -r metapackage-repo --url $MY_REG/metapackage-repo:1.0.0 -n metapackage-install
