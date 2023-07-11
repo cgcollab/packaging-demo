8360718c04d55545b316d4540170ad26c934046f
Cora Iberkleid <ciberkleid@gmail.com>
2023-07-11 03:33:51 -0400

    initial commit

---
+++ scripts/00-start-state.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+if [[ $(basename $(pwd)) == "scripts" ]]; then cd .. && echo "PWD=$(pwd)"; fi
+
+rm -rf tmp/
+
+rm -rf repository/1.0.0/packages
+rm -f repository/1.0.0/package-repository.yml
+rm -f repository/1.0.0/pkgrepo-build.yml
+echo '' > repository/1.0.0/.gitkeep
+
+rm -rf repository/1.0.0-small/packages
+rm -f repository/1.0.0-small/package-repository.yml
+rm -f repository/1.0.0-small/pkgrepo-build.yml
+echo '' > repository/1.0.0-small/.gitkeep
+
+rm -rf packages/giant-app/carvel-artifacts
+rm -f packages/giant-app/package-build.yml
+rm -f packages/giant-app/package-resources.yml
+
+rm -rf packages/hello-app/carvel-artifacts
+rm -f packages/hello-app/package-build.yml
+rm -f packages/hello-app/package-resources.yml
+
+rm -rf packages/hello-redis/carvel-artifacts
+rm -rf packages/hello-redis/upstream
+rm -f packages/hello-redis/package-build.yml
+rm -f packages/hello-redis/package-resources.yml
+rm -f packages/hello-redis/vendir.yml
+rm -f packages/hello-redis/vendir.lock.yml
+
+
+
